fe5da7af2d0f01f9a6e2ea6911063b6c9fa9f39d
Victoria244 <masprillai@estudiante.uniajc.edu.co>
2025-02-10 21:34:48 +0000

    Clase 2

---
+++ 12025Uniajc/clase2/Ejercicio.java
@@ -0,0 +1,13 @@
+public class Ejercicio{
+    public static void main(String[ ]args){
+        //Ares del cuadrado
+        //Declaracion de las variables
+        int lado, area;
+        //Asaignemos un valor a la variable lado
+        lado = 3;
+        //Calculas el area del cuadrado
+        area = lado * lado;
+        //Mostramos por pantalla el resultado de la formula
+        System.out.println("El area del cuadrado:" + area);
+    }
+}
