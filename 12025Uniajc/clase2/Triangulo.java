66eb48179890bf02627f017bdd0bbd3606dbcaf5
Victoria244 <masprillai@estudiante.uniajc.edu.co>
2025-03-10 19:42:43 +0000

    Solucion Ejercicio1.4

---
+++ 12025Uniajc/clase2/Triangulo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Triangulo{
+}
+public static void main(String[ ]args){
+    //Crear un objeto Scanner para leer la entrada del usuario
+    Scanner scanner = new Scanner(System.in);
+
+    // Solicitar la base del triangulo
+    System.out.print("Ingrese la base del triangulo:");
+    double base = scanner.nextDouble();
+
+    //Solicitar la altura del triangulo
+    System.out.println();
+}
+    
+
