1f764d07f45cd715d82a313c19d559f3e8a26a31
Victoria244 <masprillai@estudiante.uniajc.edu.co>
2025-03-23 21:46:25 +0000

    soluciondelEjercicio1punto4D

--- Actividad2.12/clase7/Ejercicio4punto1C.java
+++ Actividad2.12/clase7/Ejercicio4punto1C.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
-public class Ejercicio4punto1C { 
+public class Ejercicio4punto1C {
     public static void main(String[] args) {
        
         Scanner leer = new Scanner(System.in);
 
         double positivos = 20; 
         double negativos = -10;
-        double numero;
-        System.out.print("Introduce un nuemro :");
+        double numero = 0;
+
+        //Mensaje de peticion de ingreso de numero del usuario
+        System.out.print("Introduce un numero :");
         numero = leer.nextDouble();
 
         if (numero > 0){
-            System.out.println(" total de positivos" + positivos + numero);
+            System.out.println(" total de positivos: " + positivos + numero);
         }else{
-            System.out.println("total de negativos" + positivos + numero);
+            System.out.println("total de negativos: " + negativos + numero);
 
             leer.close();
             
